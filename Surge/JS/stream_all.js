476106c383145031c0abe8da60561337a0e5bcab
usrnb <49484576+usrnb@users.noreply.github.com>
2022-05-20 03:59:50 +0800

    Update stream_all.js

--- Surge/JS/stream_all.js
+++ Surge/JS/stream_all.js
@@ -20,7 +20,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
 
   ;(async () => {
     let panel_result = {
-      title: '流媒体解锁检测',
+      title: '流媒体解锁',
       content: '',
       icon: 'play.circle',
       'icon-color': '#67CE67',
